d144acbf4e7d9217e4c9d16a024e84ffc9f82bd9
keal khondekar <kewalkhondekar25@gmail.com>
2024-07-02 01:27:11 +0530

    feat: formik validations added

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup"
 
 const signUpSchema = yup.object({
@@ -11,20 +11,48 @@ const signUpSchema = yup.object({
 
 const SignUp = () => {
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: ""
-    },
-    validationSchema: signUpSchema,
-    onSubmit: (values) => console.log(values)
-  })
   return (
-    <section>
-      <input type="text" placeholder='name'/>
-      <input type="text" placeholder='email'/>
-      <input type="text" placeholder='password'/>
+    <section className='flex flex-col justify-center place-items-center h-screen'>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={yup.object({
+          name: yup.string().required().min(3, "Minimum 3 Charecters Required"),
+          email: yup.string().email().required(),
+          password: yup.string().required(),
+        })}
+        onSubmit={values => handleRegister(values)}>
+        <Form>
+          {
+            <div>
+              <dl>
+                <dt>Your Name</dt>
+                <dd>
+                  <Field type="text" name="name" placeholder='Name'
+                    className='bg-[#161921] border' />
+                </dd>
+                <dd className='text-red-500'><ErrorMessage name="name" /></dd>
+                <dt>Your Email</dt>
+                <dd>
+                  <Field type="text" name="email" placeholder='Email'
+                    className='bg-[#161921] border' />
+                </dd>
+                <dd className='text-red-500'><ErrorMessage name="email" /></dd>
+                <dt>Your Password</dt>
+                <dd>
+                  <Field type="text" name="password" placeholder='Password'
+                    className='bg-[#161921] border' />
+                </dd>
+                <dd className='text-red-500'><ErrorMessage name="password" /></dd>
+              </dl>
+              <button className='bg-slate-50 text-black'>SignUp</button>
+            </div>
+          }
+        </Form>
+      </Formik>
     </section>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <main className='bg-[#10141B] text-white'>
     <App />
-  </React.StrictMode>,
+  </main>,
 )
